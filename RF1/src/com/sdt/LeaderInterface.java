9f55a5d2821c358d7a41d32c7d62c285448c6cb8
DanielVale20246 <valedaniel2002@sapo.pt>
2024-12-13 23:59:28 +0000

    Corrigir eleição

--- RF1/src/com/sdt/LeaderInterface.java
+++ RF1/src/com/sdt/LeaderInterface.java
@@ -4,6 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public interface LeaderInterface extends Remote {
     void addNode(String nodeId) throws RemoteException;
@@ -11,6 +12,8 @@ public interface LeaderInterface extends Remote {
     Map<String, String> getDocumentVersions() throws RemoteException;  // Método para obter versões de documentos
     List<String> getPendingUpdates() throws RemoteException;            // Método para obter atualizações pendentes
     void receiveAck(String documentId, String nodeId) throws RemoteException;  // Para o recebimento de ACKs
-
+    void setNewLeader(String nodeId) throws RemoteException;
     void updateAckTime(String nodeId) throws RemoteException;
+
+    CopyOnWriteArrayList<String> getActiveNodes() throws RemoteException;
 }
