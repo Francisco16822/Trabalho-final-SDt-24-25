2790a20ff8a99563cc96ca1a874e90bd59e0f9ef
DanielVale20246 <valedaniel2002@sapo.pt>
2024-12-26 15:48:44 +0000

    Revert "Reapply "Melhorias mas ainda falta""
    This reverts commit 52d58acb80ab3acf9dd8f4a0ec47a1ae46b1e682.

--- RF1/src/com/sdt/LeaderInterface.java
+++ RF1/src/com/sdt/LeaderInterface.java
@@ -11,11 +11,9 @@ public interface LeaderInterface extends Remote {
     void updateDocument(String documentId, String content) throws RemoteException;
     Map<String, String> getDocumentVersions() throws RemoteException;  // Método para obter versões de documentos
     List<String> getPendingUpdates() throws RemoteException;            // Método para obter atualizações pendentes
-
     void receiveAck(String documentId, String nodeId) throws RemoteException;  // Para o recebimento de ACKs
     void setNewLeader(String nodeId) throws RemoteException;
     void updateAckTime(String nodeId) throws RemoteException;
-    CopyOnWriteArrayList<String> getActiveNodes() throws RemoteException;
-    public long updateHeartbeatTime(String nodeId) throws RemoteException;
 
+    CopyOnWriteArrayList<String> getActiveNodes() throws RemoteException;
 }
