2fb6f064e7eb2772f41a5660b0e3cac3aed17b7a
DanielVale20246 <valedaniel2002@sapo.pt>
2024-12-26 15:48:54 +0000

    Revert "Reapply "Corrigir eleição""
    This reverts commit 5a78d68854f0857ea54cfb9da5f84eac97895d5f.

--- RF1/src/com/sdt/LeaderInterface.java
+++ RF1/src/com/sdt/LeaderInterface.java
@@ -4,7 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public interface LeaderInterface extends Remote {
     void addNode(String nodeId) throws RemoteException;
@@ -12,8 +11,6 @@ public interface LeaderInterface extends Remote {
     Map<String, String> getDocumentVersions() throws RemoteException;  // Método para obter versões de documentos
     List<String> getPendingUpdates() throws RemoteException;            // Método para obter atualizações pendentes
     void receiveAck(String documentId, String nodeId) throws RemoteException;  // Para o recebimento de ACKs
-    void setNewLeader(String nodeId) throws RemoteException;
-    void updateAckTime(String nodeId) throws RemoteException;
 
-    CopyOnWriteArrayList<String> getActiveNodes() throws RemoteException;
+    void updateAckTime(String nodeId) throws RemoteException;
 }
